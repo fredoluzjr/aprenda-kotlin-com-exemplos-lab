5afbc275eb00559890c7184e5432f091172551fa
Alfredo Luz Junior <89620134+fredoluzjr@users.noreply.github.com>
2022-12-16 12:26:37 -0300

    Ajuste nos valores de enum Nivel e classe Usuario

--- desafio.kt
+++ desafio.kt
@@ -1,8 +1,6 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
+data class Usuario(val nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
