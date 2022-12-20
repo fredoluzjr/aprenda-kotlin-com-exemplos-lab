631bafa03baef2f66a4fe8be63c2de3b2446ff19
Alfredo Luz Junior <89620134+fredoluzjr@users.noreply.github.com>
2022-12-19 22:30:45 -0300

    Comentarios para estruturar o desafio

--- desafio.kt
+++ desafio.kt
@@ -1,14 +1,18 @@
+// explorar a sintaxe em orientação a objetos
+
 enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-data class Usuario(val nome: String)
+data class Usuario(val nome: String, val email: String)
+// habilidades e objetivos (array string)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int)
+// tecnologias (array string), nivel
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
+    fun matricular(usuario: Usuario) { // vararg para poder inscrever um ou varios usuarios
         TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
     }
 }
