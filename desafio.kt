d8bdbb3ecce76159aa8b9051fff725d59c2706bd
Alfredo Luz Junior <89620134+fredoluzjr@users.noreply.github.com>
2022-12-21 12:31:18 -0300

    Testes de cadastro de alunos

--- desafio.kt
+++ desafio.kt
@@ -5,20 +5,28 @@ enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 data class Usuario(val nome: String, val email: String)
 // habilidades e objetivos (array string) -> Implementação futura
 
-data class ConteudoEducacional(val nome: String, val nivel: Nivel, val duracao: Int) : Formacao(nome = nome, conteudos = conteudos) 
-// ConteudoEducacional faz parte da classe Formacao
+data class ConteudoEducacional(val nome: String, val nivel: Nivel, val duracao: Int)
 // tecnologias (array string) -> Implementação futura
 
-open class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) { // Formacao é uma Superclasse
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(vararg usuario: Usuario) { // vararg para poder inscrever um ou varios usuarios
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val aluno1 = Usuario("Jorge", "jorge@email.com")
+    val aluno2 = Usuario("Ana", "ana@gmail.com")
+    val aluno3 = Usuario("Caio", "caio@gmail.com")
+    
+    println(aluno1)
+    println(aluno2)
+    println(aluno3)
+    //println(inscritos)
 }
